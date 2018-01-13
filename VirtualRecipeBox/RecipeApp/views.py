6c32ce9aa6d76ab653916dd7e086d0ca303fb16a
sebastian.teodorescu <sebastian.teodorescu13@gmail.com>
2018-01-13 13:02:24 +0200

    - myrecipes html - myrecipes ew - urls & layout update

--- VirtualRecipeBox/RecipeApp/views.py
+++ VirtualRecipeBox/RecipeApp/views.py
@@ -2,11 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import (TemplateView, ListView,)
+from django.views.generic import (TemplateView, ListView, CreateView)
 from RecipeApp.models import Recipe, Favourite
 # Create your views here.
 
-from .forms import AddRecipeForm
+#from .forms import AddRecipeForm
 from .models import Favourite, Recipe
 
 
@@ -30,8 +30,15 @@ class FavouritesListView(ListView):
         return Favourite.objects.all()
 
 
-class RecipeCreateView(LoginRequiredMixin, CreateView):
-    template_name = 'addrecipe.html'
-    form_class = AddRecipeForm
-    model = Recipe
+# class RecipeCreateView(CreateView):
+#     template_name = 'addrecipe.html'
+#     form_class = AddRecipeForm
+#     model = Recipe
 
+class MyRecipesView(ListView):
+    template_name = 'myrecipes.html'
+    model = Recipe
+    context_object_name = 'myrecipes'
+
+    def get_queryset(self, *args, **kwargs):
+        return Recipe.objects.all()
